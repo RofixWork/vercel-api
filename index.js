b2e7ba99f707880bd6b4d49396e1cedc4ce0e72f
rofix kengan <wourkout123@gmail.com>
2022-03-15 02:51:34 +0100

    f

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const express = require("express");
 const app = express();
 
 app.get("/", (req, res) => {
-  res.send("Express on Vercel");
+  res.json("Express on Vercel");
 });
 
 app.listen(5000, () => {
